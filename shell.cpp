c2fef06d5e9b6d05b846c59adb583067f3e89cff
Moriah Hammond <moriah.hammond@snagajob.com>
2017-10-30 20:37:27 -0400

    Added blank file

---
+++ shell.cpp
@@ -0,0 +1,16 @@
+int main (int argc, char **argv) {
+
+//Load configuration files
+
+//Run command loop
+  lsh_loop();
+
+//Perform any shutdown/cleanup
+
+  return EXIT_SUCCESS;
+
+}
+
+void lsh_loop(void){
+  
+}
