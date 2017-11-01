0b448ff38bb2ab8d89d97bbeec2f6a63f110ed89
samsonjj <samsonjj@vcu.edu>
2017-10-31 20:48:08 -0400

    Create basic_shell.cpp

---
+++ basic_shell.cpp
@@ -0,0 +1,47 @@
+// Basic shell...
+// Runs a loop reading line input into string
+// Processes string based on process_input(std::string input) method
+
+#include <string>
+#include <iostream>
+
+std::string input_line = "";
+
+int process_input(std::string input) {
+
+	if (input == "EXEC") {
+		std::cout << "executing..." << std::endl;
+		std::cout << "done." << std::endl;
+		return EXIT_SUCCESS;
+	}
+	if (input == "LOAD") {
+		std::cout << "loading..." << std::endl;
+		std::cout << "done." << std::endl;
+		return EXIT_SUCCESS;
+	}
+	if (input == "EXIT") {
+		std::cout << "exiting..." << std::endl;
+		exit(0);
+		return EXIT_SUCCESS;
+	}
+	else {
+		std::cout << "invalid command." << std::endl;
+		return EXIT_SUCCESS;
+	}
+}
+
+void shell_loop() {
+	while (true) {
+		std::cout << "command: ";
+		std::getline(std::cin, input_line);
+		process_input(input_line);
+	}
+}
+
+int main()
+{
+	shell_loop();
+}
+
+
+
